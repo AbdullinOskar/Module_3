ec79bc8c7fcb97cc6539a40c15f9c8f95f16483d
AbdullinOskar <106113902+AbdullinOskar@users.noreply.github.com>
2024-03-28 21:08:38 +0300

    Add files via upload

---
+++ Module_3_HTML_DOM.py
@@ -0,0 +1,12 @@
+def test(a = 1, b = "Hi", c = 22):
+    print(a,b,c)
+
+test()
+
+def factorial(n):
+    if n == 1:
+        return 1
+    fac_n_min_1 = factorial(n=n-1)
+    return n * fac_n_min_1
+
+print(factorial(5))
